b59c76ff352ab5df6425da833d47f83e1c50cf1d
victorhugofmalaguetta-arch <victorhugo.fmalaguetta@gmail.com>
2026-03-11 17:29:42 -0300

    Update bot-authme-first.js

--- bot-authme-first.js
+++ bot-authme-first.js
@@ -1,38 +1,31 @@
 const mineflayer = require('mineflayer')
 
+function iniciarBot() {
+
 const bot = mineflayer.createBot({
   host: 'arcanumcraft.com.br',
   port: 25565,
   username: 'feliz',
-  version: '1.21.1'
+  version: false
 })
 
 bot.on('spawn', () => {
-  console.log('Bot entrou')
-
-
 
   setTimeout(() => {
-    bot.setControlState('forward', false)
-  }, 2000)
-
-  // login
-  setTimeout(() => {
-    console.log('Mandando login')
     bot.chat('/login farmbmb')
   }, 5000)
 
-  // register caso precise
   setTimeout(() => {
-    bot.chat('/register farmbmb farmbmb')
-  }, 8000)
-
+    bot.chat('/home 1')
+  }, 9000)
 
 })
 
-bot.on('message', (msg) => {
-  console.log(msg.toString())
+bot.on('end', () => {
+  console.log('Bot caiu... reconectando em 10s')
+  setTimeout(iniciarBot, 10000)
 })
 
-bot.on('error', console.log)
-bot.on('kicked', console.log)
+}
+
+iniciarBot()
